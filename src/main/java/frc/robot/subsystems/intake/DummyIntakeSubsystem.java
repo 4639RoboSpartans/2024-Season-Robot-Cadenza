6f604e3262012a5ef0800d36495cfe2f62592282
Jonathan Jiang <89473479+Nengyi-Jonathan-Jiang@users.noreply.github.com>
2024-01-13 17:58:06 -0600

    Update DummyIntakeSubsystem.java

--- src/main/java/frc/robot/subsystems/intake/DummyIntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/intake/DummyIntakeSubsystem.java
@@ -4,5 +4,5 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DummyIntakeSubsystem extends SubsystemBase implements IIntakeSubsystem {
     @Override
-    public void intake() {}
+    public void setIntakeDegree(double degree) {}
 }
