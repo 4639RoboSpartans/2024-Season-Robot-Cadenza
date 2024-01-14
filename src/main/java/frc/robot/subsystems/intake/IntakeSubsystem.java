430f2e5be331711a9994ee487b92b9a20e23550c
Jonathan Jiang <89473479+Nengyi-Jonathan-Jiang@users.noreply.github.com>
2024-01-13 18:00:00 -0600

    Add files via upload

---
+++ src/main/java/frc/robot/subsystems/intake/IntakeSubsystem.java
@@ -0,0 +1,35 @@
+package frc.robot.subsystems.intake;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class IntakeSubsystem extends SubsystemBase implements IIntakeSubsystem{
+    private final TalonFX intakeMotor;
+    private final TalonFX pivotMotor;
+
+    private final PIDController pivotPID;
+
+    public IntakeSubsystem(int intakeMotorID, int pivotID){
+        intakeMotor = new TalonFX(intakeMotorID);
+        pivotMotor = new TalonFX(pivotID);
+
+        pivotPID = Constants.RobotInfo.SHOOTER_AIM_PID.create();
+
+        intakeMotor.setNeutralMode(NeutralModeValue.Brake);
+        pivotMotor.setNeutralMode(NeutralModeValue.Coast);
+    }
+
+    //Pivots the intake to "retract" and "extend" intake
+    public void setIntakeDegree(double degrees) {
+        pivotPID.setSetpoint(degrees);
+    }
+
+    //Spins intake motor to intake notes
+    public void intake(){
+        intakeMotor.set(Constants.RobotInfo.INTAKE_SPEED);
+    }
+}
