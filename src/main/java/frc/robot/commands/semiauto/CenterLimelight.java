1a6be74036cd3d8b9f73a8a5ce56f1e2d9617aa7
mjjbycicle <60635308+mjjbycicle@users.noreply.github.com>
2024-02-02 21:55:31 -0600

    Update CenterLimelight.java

--- src/main/java/frc/robot/commands/semiauto/CenterLimelight.java
+++ src/main/java/frc/robot/commands/semiauto/CenterLimelight.java
@@ -2,6 +2,7 @@ package frc.robot.commands.semiauto;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants;
 import frc.robot.network.LimeLight;
@@ -10,12 +11,13 @@ import math.MathUtil;
 
 import java.util.ArrayDeque;
 
+record RobotPose(double xOffset, double zOffset, double yRotation) {}
+
 public class CenterLimelight extends Command {
     private final SwerveDriveSubsystem swerveDrive;
-    private final ArrayDeque<Double> prevPoses = new ArrayDeque<Double>();
-
+    private final ArrayDeque<RobotPose> prevPoses = new ArrayDeque<>();
     // TODO: move pid into Constants.java
-    private final PIDController rotationPID = new PIDController(0.375, 0.0002, 0.03);
+    private PIDController rotationPID = new PIDController(0.5, 0.0002, 0.03);
 
     public CenterLimelight(SwerveDriveSubsystem swerveDriveSubsystem) {
         this.swerveDrive = swerveDriveSubsystem;
@@ -29,6 +31,7 @@ public class CenterLimelight extends Command {
         prevPoses.clear();
         rotationPID.reset();
         rotationPID.setSetpoint(0);
+        SmartDashboard.putNumber("rotatorPID kD", 0);
     }
 
     @Override
@@ -37,22 +40,32 @@ public class CenterLimelight extends Command {
 
         if(prevPoses.isEmpty()) return;
 
-        double yRotation = prevPoses.stream().mapToDouble(Double::doubleValue).sum() / prevPoses.size();
+        double yRotation = prevPoses.stream().mapToDouble(RobotPose::yRotation).sum() / prevPoses.size();
 
-        double yRtSpd = rotationPID.calculate(MathUtil.signedPow(yRotation, 0.7)) * 2.3;
+        double yRtSpd = rotationPID.calculate(MathUtil.signedPow(yRotation, 0.7)) * Constants.RobotInfo.MAX_ROBOT_SPEED;
 
         swerveDrive.setRawMovement(new ChassisSpeeds(0, 0, yRtSpd));
+
+        double kD = 1/Math.pow(Math.abs(SmartDashboard.getNumber("AprilTag: y rotation", 1))/5, 2);
+        if (kD > 10) kD = 0.01;
+
+        rotationPID.setD(kD);
+        SmartDashboard.putNumber("rotatorPID kD", rotationPID.getD());
     }
 
     private void acceptInput() {
+        double xOffset = LimeLight.getXDistance();
+        double zOffset = LimeLight.getZDistance() - Constants.FieldDistances.ShooterApriltagZDistance;
+//        double yRotation = LimeLight.getYRotation();
+
         double angle = -Math.toRadians(LimeLight.getTx());
 
-        if(angle == 0) return;
+        if(xOffset == 0) return;
 
         if(prevPoses.size() >= Constants.CENTER_LIMELIGHT_AVERAGING_WINDOW_LENGTH) {
             prevPoses.removeFirst();
         }
-        prevPoses.addLast(angle);
+        prevPoses.addLast(new RobotPose(xOffset, zOffset, angle));
     }
 
     @Override
