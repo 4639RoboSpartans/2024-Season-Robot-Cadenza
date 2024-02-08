71d57faba2b33afd1c617c2a91262a8692ddf2d3
Hephaestus <programming@team4639.org>
2024-02-07 19:17:42 -0600

    made ManualSwerveSubsystem compatible with centering limelight

---
+++ src/main/java/frc/robot/subsystems/swerve/RotationPIDSubsystem.java
@@ -0,0 +1,60 @@
+package frc.robot.subsystems.swerve;
+
+import java.util.ArrayDeque;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.network.LimeLight;
+import frc.robot.oi.OI;
+import math.MathUtil;
+
+record RobotPose(double xOffset, double zOffset, double yRotation) {}
+
+public class RotationPIDSubsystem extends SubsystemBase{
+    private boolean aIsPressed = false;
+    private final OI oi;
+    private final PIDController rotationPID = Constants.RobotInfo.ROTATION_PID.create();
+    private final ArrayDeque<RobotPose> prevPoses = new ArrayDeque<>();
+    public RotationPIDSubsystem(OI oi){
+        this.oi = oi;
+    }
+
+
+    public double getRotation(){
+        acceptInput();
+    
+        if (oi.getDriverController().getButton(OI.Buttons.A_BUTTON).getAsBoolean() == false){
+            aIsPressed = false;
+            return -oi.getDriverController().getAxis(Constants.Controls.SwerveRotationAxis);
+        }
+        
+        if (prevPoses.isEmpty()) 
+            return -oi.getDriverController().getAxis(Constants.Controls.SwerveRotationAxis);
+        
+        if (oi.getDriverController().getButton(OI.Buttons.A_BUTTON).getAsBoolean()){
+            if (aIsPressed == false){
+                aIsPressed = true;
+                rotationPID.reset();
+            }
+        }
+
+        double yRotation = prevPoses.stream().mapToDouble(RobotPose::yRotation).sum() / prevPoses.size();
+        double yRtSpd = rotationPID.calculate(MathUtil.signedPow(yRotation, 0.7)) * Constants.RobotInfo.MAX_ROBOT_SPEED;
+        return yRtSpd;
+    }
+
+    private void acceptInput() {
+        double xOffset = LimeLight.getXDistance();
+        double zOffset = LimeLight.getZDistance() - Constants.FieldDistances.ShooterApriltagZDistance;
+
+        double angle = -Math.toRadians(LimeLight.getTx());
+
+        if(xOffset == 0) return;
+
+        if(prevPoses.size() >= Constants.CENTER_LIMELIGHT_AVERAGING_WINDOW_LENGTH) {
+            prevPoses.removeFirst();
+        }
+        prevPoses.addLast(new RobotPose(xOffset, zOffset, angle));
+    }
+}
