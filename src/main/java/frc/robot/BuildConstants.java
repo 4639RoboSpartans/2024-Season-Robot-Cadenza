ddb8fa606d37b96d3881a543075bb55059eff598
Michael Jiang <nengzhimj@gmail.com>
2024-05-29 12:40:57 -0500

    .

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2024-Season-Robot-Cadenza";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 279;
-  public static final String GIT_SHA = "425be327da877be416917a347a623d6234d45bb6";
-  public static final String GIT_DATE = "2024-05-23 20:56:20 EDT";
+  public static final int GIT_REVISION = 280;
+  public static final String GIT_SHA = "bdbac727bc0f1ea2e2d75e51058880fb5ec12e9f";
+  public static final String GIT_DATE = "2024-05-29 13:39:18 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2024-05-29 13:37:58 EDT";
-  public static final long BUILD_UNIX_TIME = 1717004278348L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2024-05-29 13:40:43 EDT";
+  public static final long BUILD_UNIX_TIME = 1717004443228L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
