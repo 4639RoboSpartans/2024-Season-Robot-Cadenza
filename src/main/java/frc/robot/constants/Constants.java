d8840ae6d11003488660ba343561b64282330c27
Michael Jiang <nengzhimj@gmail.com>
2024-05-29 13:04:16 -0500

    .

--- src/main/java/frc/robot/constants/Constants.java
+++ src/main/java/frc/robot/constants/Constants.java
@@ -5,20 +5,18 @@ public final class Constants {
     public static final int POSE_WINDOW_LENGTH = 1;
     public static final double INTAKE_PIVOT_UP_MULTIPLIER = 2;
 
-
     public enum CurrentRobot {
-        ZEUS, SIREN
+        ZEUS,
+        SIREN
     }
+
     public static final CurrentRobot currentRobot = CurrentRobot.SIREN;
 
-    private static final double[] oldOffsets = {
-            -54.98, -122.4, 74.44, 121.92
-    };
-    private static final double[] newOffsets = {
-            135.36, 155.23, -94.98, -88.23
-    };
-    static final double[] swerveOffsets = switch (currentRobot) {
-        case ZEUS -> oldOffsets;
-        case SIREN -> newOffsets;
-    };
+    private static final double[] oldOffsets = {-54.98, -122.4, 74.44, 121.92};
+    private static final double[] newOffsets = {135.36, 155.23, -94.98, -88.23};
+    static final double[] swerveOffsets =
+            switch (currentRobot) {
+                case ZEUS -> oldOffsets;
+                case SIREN -> newOffsets;
+            };
 }
