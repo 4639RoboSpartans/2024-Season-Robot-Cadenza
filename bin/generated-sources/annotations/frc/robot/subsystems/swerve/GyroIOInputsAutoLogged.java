bef53c86087a692e5802a32d25d0d9dffe772c37
Michael <nengzhimj@gmail.com>
2024-06-03 09:33:10 -0500

    fixing autos

--- bin/generated-sources/annotations/frc/robot/subsystems/swerve/GyroIOInputsAutoLogged.java
+++ bin/generated-sources/annotations/frc/robot/subsystems/swerve/GyroIOInputsAutoLogged.java
@@ -1,11 +1,10 @@
 package frc.robot.subsystems.swerve;
 
-import java.lang.Cloneable;
-import java.lang.Override;
 import org.littletonrobotics.junction.LogTable;
 import org.littletonrobotics.junction.inputs.LoggableInputs;
 
-public class GyroIOInputsAutoLogged extends GyroIO.GyroIOInputs implements LoggableInputs, Cloneable {
+public class GyroIOInputsAutoLogged extends GyroIO.GyroIOInputs
+    implements LoggableInputs, Cloneable {
   @Override
   public void toLog(LogTable table) {
     table.put("Connected", connected);
--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -3,14 +3,14 @@ package frc.robot;
 /** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "2024-Season-Robot";
+  public static final String MAVEN_NAME = "2024-Season-Robot-Cadenza";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 287;
-  public static final String GIT_SHA = "42cf79eca647aca13c6de20be3c2db8016a1a608";
-  public static final String GIT_DATE = "2024-05-30 21:17:16 EDT";
+  public static final int GIT_REVISION = 296;
+  public static final String GIT_SHA = "b686ff27c517281b409687a223444e70c601d4c5";
+  public static final String GIT_DATE = "2024-06-03 08:27:30 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2024-05-30 21:56:16 EDT";
-  public static final long BUILD_UNIX_TIME = 1717120576354L;
+  public static final String BUILD_DATE = "2024-06-03 10:13:04 EDT";
+  public static final long BUILD_UNIX_TIME = 1717423984597L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
