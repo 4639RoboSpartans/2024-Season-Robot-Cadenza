c74e6ee06e98505b7d5a61c1a705b719b6c89665
Michael Jiang <nengzhimj@gmail.com>
2024-06-04 16:39:40 -0500

    transition to akit-style io

---
+++ src/main/java/frc/robot/subsystems/shooter/pivot/ShooterPivotIO.java
@@ -0,0 +1,2 @@
+package frc.robot.subsystems.shooter.pivot;public interface ShooterPivotIO {
+}
---
+++ src/main/java/frc/robot/subsystems/shooter/pivot/ShooterPivotIOSparkMax.java
@@ -0,0 +1,6 @@
+package frc.robot.subsystems.shooter.pivot;
+
+import frc.robot.subsystems.shooter.ShooterIO;
+
+public class ShooterPivotIONeo implements ShooterIO {
+}
